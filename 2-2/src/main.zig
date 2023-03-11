f6ffc590ec500b9f2a58b395dd39737482b63db5
Johannes Qvarford <jq.email+gitlab@pm.me>
2023-03-11 16:16:03 +0100

    2-2

--- 2-2/src/main.zig
+++ 2-2/src/main.zig
@@ -25,36 +25,40 @@ fn parseCommand(parser: *p.Parser) !Command {
     };
 }
 
-const Position = struct {
+const State = struct {
     x: i32,
     y: i32,
+    aim: i32,
 
     const Self = @This();
 
-    fn start() Position {
+    fn start() State {
         return Self {
             .x = 0,
-            .y = 0
+            .y = 0,
+            .aim = 0
         };
     }
 
-    fn advance(self: Self, command: Command) !Position {
-        const Vector = struct { x: i2, y: i2 };
-        const vector: Vector = switch (command.direction) {
-            .backward => Vector{ .x = -1, .y = 0},
-            .forward => Vector{ .x = 1, .y = 0},
-            .up => Vector{ .x = 0, .y = -1},
-            .down => Vector{ .x = 0, .y = 1}
+    fn advance(self: Self, command: Command) !State {
+        const Change = struct { x: i2, y: i32, aim: i32 };
+        const vector: Change = switch (command.direction) {
+            .backward => Change{ .x = -1, .y = 0, .aim = 0 },
+            .forward => Change{ .x = 1, .y = self.aim, .aim = 0 },
+            .up => Change{ .x = 0, .y = 0, .aim = -1 },
+            .down => Change{ .x = 0, .y = 0, .aim = 1 }
         };
 
         var x = try m.mulWithOverflow(vector.x, command.amount);
         x = try m.addWithOverflow(x, self.x);
         var y = try m.mulWithOverflow(vector.y, command.amount);
         y = try m.addWithOverflow(y, self.y);
+        const aim = try m.addWithOverflow(self.aim, try m.mulWithOverflow(vector.aim, command.amount));
 
         return Self {
             .x = x,
-            .y = y
+            .y = y,
+            .aim = aim
         };
     }
 };
@@ -63,7 +67,7 @@ const example = @embedFile("EXAMPLE");
 const input = @embedFile("INPUT");
 
 fn run(parser: *p.Parser) !i32 {
-    var position = Position.start();
+    var position = State.start();
     loop: while (true) {
         const command = parseCommand(parser) catch |e| {
             switch (e) {
@@ -83,29 +87,11 @@ test {
 test "example" {
     var parser = p.Parser { .buffer = example };
     const actual: i32 = try run(&parser);
-    try std.testing.expectEqual(@as(i32, 150), actual);
+    try std.testing.expectEqual(@as(i32, 900), actual);
 }
 
 test "input" {
     var parser = p.Parser { .buffer = input };
     const actual: i32 = try run(&parser);
-    try std.testing.expectEqual(@as(i32, 1746616), actual);
-}
-
-test "valid command" {
-    var parser = p.Parser { .buffer = "forward 5\n" };
-    const actual: i32 = try run(&parser);
-    try std.testing.expectEqual(@as(i32, 0), actual);
-}
-
-test "valid command no line ending" {
-    var parser = p.Parser { .buffer = "forward 5" };
-    const actual: i32 = try run(&parser);
-    try std.testing.expectEqual(@as(i32, 0), actual);
-}
-
-test "valid commands" {
-    var parser = p.Parser { .buffer = "forward 5\ndown 4" };
-    const actual: i32 = try run(&parser);
-    try std.testing.expectEqual(@as(i32, 20), actual);
+    try std.testing.expectEqual(@as(i32, 1741971043), actual);
 }
